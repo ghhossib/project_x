5ee768403dbb0d6bcb5a6f53a838971ace822319
wEm1Xxx <sergeygevkan352006@gmail.com>
2025-04-09 12:22:17 +0500

    123

--- Controllers/GroupsController.py
+++ Controllers/GroupsController.py
@@ -16,32 +16,20 @@ class GroupsController:
         return Groups.get_or_none(id)
 
 
-
-@classmethod
-def show(cls, id):
-    return Groups.get_or_none(id)
-
-
-@classmethod
-def add(cls, name):
-    Groups.create(name=name)
+    @classmethod
+    def add(cls, name):
+        Groups.create(name=name)
 
 
-
-@classmethod
-def get(cls):
-    return Groups.select()
-
-
-@classmethod
-def update(cls, id, **filds):
-    for key, value in filds.items():
-        Groups.update({key: value}).where(Groups.id == id).execute()
+    @classmethod
+    def update(cls, id, **filds):
+        for key, value in filds.items():
+            Groups.update({key: value}).where(Groups.id == id).execute()
 
 
-@classmethod
-def delete(cls, id):
-    Groups.delete_by_id(id)
+    @classmethod
+    def delete(cls, id):
+        Groups.delete_by_id(id)
 
 
 if __name__ == "__main__":
